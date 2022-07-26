aac920f3510189749681025d4efc2a1b884435d9
camilogranda <camilo.granda96@gmail.com>
2022-07-26 01:11:09 -0500

    add two files

---
+++ git_commands.c
@@ -0,0 +1,21 @@
+int main (void)
+{
+    printf("Hola Mundo");
+    //git status
+
+    //git clone <repo>
+    //git fetch -> Descarga metadata github
+    //git pull origin main
+    //git push origin main
+    //git commit -am "mensaje"
+    //cd .. -> devolverse de carpeta
+    //git checkout HEAD -- <archivo a reiniciar al último commit>
+    //git checkout HEAD~1 -> ir un commit atrás
+    //git add <archivo a agregar>
+    //git add . -> "add files in the current directory"
+    //git log -> historia de commits
+    //git log --oneline -> historia reducida de commits
+    //git log --oneline --graph -> historia con gráfico de commits
+    //git rm -cached <file> -> unstage a file
+    
+}
